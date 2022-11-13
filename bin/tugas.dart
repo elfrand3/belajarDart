3b9cd6907e6113ce377b0a5359fd01e69e15dd5d
el_codee <elfrand3@gmail.com>
2022-11-13 22:28:26 +0700

    update

--- bin/tugas.dart
+++ bin/tugas.dart
@@ -4,8 +4,12 @@ import 'dart:io';
 void main(List<String> arguments) {
 
   for (int i = 1; i <= 15; i++){
-    if ( i == 3){
+    if ( i == 3 || i == 6 || i == 9 || i == 12){
       print("Fizz ");
+    }else if ( i == 4 || i == 8){
+      print("Buzz ");
+    }else if ( i == 15){
+      print("FizzBuzz ");
     }else{
       print("$i ");
     }
